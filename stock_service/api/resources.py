bbada2fca9dc7e0717a16fd91df4ef32b9672584
morales-roman <86542664+morales-roman@users.noreply.github.com>
2023-11-03 21:28:37 -0600

    request and validate API call

--- stock_service/api/resources.py
+++ stock_service/api/resources.py
@@ -1,7 +1,8 @@
 # encoding: utf-8
 
-from flask import request
+from flask import request, current_app
 from flask_restful import Resource
+import requests
 
 from stock_service.api.schemas import StockSchema
 
@@ -12,10 +13,28 @@ class StockResource(Resource):
     them to our main API service. Currently we only get the data from a single external source:
     the stooq API.
     """
-
     def get(self):
-        # TODO: Implement the call to the stooq service here. The stock code to query the API
-        # should come in a query parameter.
-        stock_data_obj = None
+        """
+        Retrieve stock data from the stooq service based on the stock code provided in the query parameter.
+
+        Returns:
+            A serialized representation of the stock data object.
+        """
+        
+        stock_code = request.args.get('q')
+        if not stock_code:
+            return {'API Error': 'Missing stock code'}, 400  
+        
+        stooq_url = current_app.config['STOOQ_API_URL'].format(stock_code)
+        stock_data_obj = requests.get(stooq_url)
+        stock_data_obj.raise_for_status() 
+
+        # Since the stooq API returns a list of objects, we only take the first one
+        stock_data_obj = stock_data_obj.json()['symbols'][0]
+
+        if not stock_data_obj.get('name'):
+            return {'API Error': 'Invalid stock code'}, 400
+        
         schema = StockSchema()
+        
         return schema.dump(stock_data_obj)
