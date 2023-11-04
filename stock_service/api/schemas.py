dcc8f3bbdac7134246a42eed4f8d56a0bd1313cc
morales-roman <86542664+morales-roman@users.noreply.github.com>
2023-11-03 21:21:15 -0600

    Added predump to parse date and time correctly

--- stock_service/api/schemas.py
+++ stock_service/api/schemas.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-from stock_service.extensions import marsh
+from stock_service.extensions import marsh, pre_dump, datetime
 
 
 class StockSchema(marsh.Schema):
@@ -13,3 +13,10 @@ class StockSchema(marsh.Schema):
     high = marsh.Float(dump_only=True)
     low = marsh.Float(dump_only=True)
     close = marsh.Float(dump_only=True)
+
+    @pre_dump
+    def format_date_and_time(self, data, **kwargs):
+        data['date'] = datetime.strptime(data['date'], '%Y-%m-%d').date()
+        data['time'] = datetime.strptime(data['time'], '%H:%M:%S').time()
+        return data
+    
