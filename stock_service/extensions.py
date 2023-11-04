7c8abb12aeab25b9d58bc53476ab399f4620580c
morales-roman <86542664+morales-roman@users.noreply.github.com>
2023-11-03 21:28:12 -0600

    more extensions added

--- stock_service/extensions.py
+++ stock_service/extensions.py
@@ -1,5 +1,9 @@
 # encoding: utf-8
 
 from flask_marshmallow import Marshmallow
+from marshmallow import pre_dump as pre_dump_
+from datetime import datetime as datetime_
 
 marsh = Marshmallow()
+datetime = datetime_
+pre_dump = pre_dump_
