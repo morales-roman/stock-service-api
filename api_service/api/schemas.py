e21aa112b7ef9413ea8e1c6ed0ad1f1c05366d47
morales-roman <86542664+morales-roman@users.noreply.github.com>
2023-11-06 01:15:03 -0600

    Refactor stock info Add history and stats schemas

--- api_service/api/schemas.py
+++ api_service/api/schemas.py
@@ -5,5 +5,19 @@ from api_service.extensions import ma
 
 class StockInfoSchema(ma.Schema):
     symbol = ma.String(dump_only=True)
-    company_name = ma.String(dump_only=True)
-    quote = ma.Float(dump_only=True)
+    name = ma.String(dump_only=True, data_key='company_name')
+    close = ma.Float(dump_only=True, data_key='quote')
+
+class StockStatsSchema(ma.Schema):
+    stock_symbol = ma.String(dump_only=True, data_key='stock')
+    times_requested = ma.Integer(dump_only=True)
+
+class HistorySchema(ma.Schema):
+    request_datetime = ma.DateTime(dump_only=True,  data_key='date')
+    name = ma.String(dump_only=True)
+    symbol = ma.String(dump_only=True)
+    open = ma.Float(dump_only=True)
+    high = ma.Float(dump_only=True)
+    low = ma.Float(dump_only=True)
+    close = ma.Float(dump_only=True)
+
