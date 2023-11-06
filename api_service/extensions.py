d53b94bdb2629a6f31f30587e0e7d267125ae0f4
morales-roman <86542664+morales-roman@users.noreply.github.com>
2023-11-06 01:12:02 -0600

    moved multiple of libraries to extensions

--- api_service/extensions.py
+++ api_service/extensions.py
@@ -5,8 +5,15 @@ from passlib.context import CryptContext
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 
+from sqlalchemy.exc import IntegrityError
+from datetime import datetime
+from sqlalchemy.orm import joinedload as jl
+
 
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
+IError = IntegrityError
+dt = datetime
+joinedload = jl
