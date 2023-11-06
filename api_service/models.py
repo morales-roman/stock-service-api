f4b090910f4f30b74d0db50ab524f4102c7367da
morales-roman <86542664+morales-roman@users.noreply.github.com>
2023-11-05 18:18:10 -0600

    added the stock history, stock entry, and stats

--- api_service/models.py
+++ api_service/models.py
@@ -5,7 +5,7 @@ from api_service.extensions import db, pwd_context
 
 
 class User(db.Model):
-    """Basic user model"""
+    __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -13,6 +13,9 @@ class User(db.Model):
     _password = db.Column("password", db.String(255), nullable=False)
     active = db.Column(db.Boolean, default=True)
     role = db.Column(db.String(20), nullable=False)
+
+    requests = db.relationship('RequestHistory', backref='user', lazy=True)
+
 
     @hybrid_property
     def password(self):
@@ -24,3 +27,52 @@ class User(db.Model):
 
     def __repr__(self):
         return "<User %s>" % self.username
+
+
+class StockEntry(db.Model):
+    __tablename__ = 'stock_entries'
+
+    id = db.Column(db.Integer, primary_key=True)
+    request_datetime = db.Column(db.DateTime, nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    symbol = db.Column(db.String(10), nullable=False)
+    open = db.Column(db.Float, nullable=False)
+    high = db.Column(db.Float, nullable=False)
+    low = db.Column(db.Float, nullable=False)
+    close = db.Column(db.Float, nullable=False)
+
+    requests = db.relationship('RequestHistory', backref='stock_entry', lazy=True)
+
+    def __repr__(self):
+        return f'<StockEntry {self.name} ({self.symbol})>'
+    
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+class RequestHistory(db.Model):
+    __tablename__ = 'requests_history'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    entries_id = db.Column(db.Integer, db.ForeignKey('stock_entries.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<RequestHistory {self.user_id} requested {self.entries_id}>'
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class StockStat(db.Model):
+    __tablename__ = 'stock_stats'
+
+    id = db.Column(db.Integer, primary_key=True)
+    stock_symbol = db.Column(db.String(10), nullable=False, unique=True)
+    times_requested = db.Column(db.Integer, default=1)
+
+    def __repr__(self):
+        return f'<StockStat {self.stock_symbol} requested {self.times_requested} times>'
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
